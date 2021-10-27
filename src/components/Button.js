97c26d47ac9bed6cd7434d54c4436508bad7a5ae
faridAbinah <faridabinah@hotmail.com>
2021-10-26 23:34:13 -0400

    Created button componenet in isolation using storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   //Use classNames to conditionally set the class name for the buttong based on if props.whatever is truthy or falsy. If truthy the key will be added as a class.
+
+   let buttonClass=  classNames('button', {'button--confirm':props.confirm},{'button--danger':props.danger});;
+   
+ 
+
+   
+   
+   
+   //disable logic
+   //clickable logic is inside the buttong tag
+   
+   return (
+      <button onClick={props.onClick} className={buttonClass} disabled={props.disabled}>{props.children}</button>
+   );
 }
